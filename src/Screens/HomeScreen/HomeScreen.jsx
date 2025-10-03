eaf846dcc576b69fff5ecf486ecf4c78d815aa23
orellanapau <orellanapaula4@gmail.com>
2025-10-03 16:55:06 -0300

    Modificación en el Home Screen

--- src/Screens/HomeScreen/HomeScreen.jsx
+++ src/Screens/HomeScreen/HomeScreen.jsx
@@ -9,7 +9,10 @@ const HomeScreen = () => {
             <NavBar/>
             <ContactList/>
             <div className='menssages-container'>
-                <span>Todavía no has seleccionado ningún contacto.</span>
+                <img className='menssages-container-img' src="https://img.freepik.com/premium-vector/hand-drawn-lemonade-cartoon-illustration_23-2150864429.jpg?w=2000" alt="Imagen principal"/>
+                <h1>Descarga Lemonade para Windows</h1>
+                <p className='menssages-container-p'>Descarga la aplicación para Windows y disfruta una experiencia más rápida</p>
+                <button>Descargar</button>
             </div>
             
         </div>
